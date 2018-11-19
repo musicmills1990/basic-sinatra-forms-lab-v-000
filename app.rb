d5672d68a6ba06d41cb14a507ed85ba28c1afe75
Marc Mills <mdmills1990@gmail.com>
2018-11-19 18:53:19 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -7,13 +7,14 @@ class App < Sinatra::Base
   end
 
   post '/newteam' do
-    "Team Name: #{params[:name]}"
-    "Coach: #{params[:coach]}"
-    "Point Guard: #{params[:pg]}"
-    "Shooting Guard: #{params[:sg]}"
-    "Small Forward: #{params[:sf]}"
-    "Power Forward: #{params[:pf]}"
-    "Center: #{params[:c]}"
+    @team_name = params[:name]
+    @coach = params[:coach]
+    @pg = params[:pg]
+    @sg = params[:sg]
+    @sf = params[:sf]
+    @pf = params[:pf]
+    @c = params[:c]
+
   end
 
 end
